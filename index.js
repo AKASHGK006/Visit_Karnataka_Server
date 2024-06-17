e35561a9d18f6420e5f9cd1951cc7b7e35ae70ff
AKASH G K <akashgk@gmail.com>
2024-06-17 10:21:56 +0530

    Fifth Commit

--- index.js
+++ index.js
@@ -24,9 +24,9 @@ app.use(express.json());
 
 // Configure Cloudinary
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET
 });
 
 const secretKey = process.env.key;
