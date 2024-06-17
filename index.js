0e39dacac47feb1cec42be4eba250f0da8890705
AKASH G K <akashgk@gmail.com>
2024-06-17 11:23:17 +0530

    Sixth Commit

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(cors({
 }));
 
 
-app.use(express.json());
+app.use(express.json({ limit: '100mb' }));
 
 const secretKey = process.env.key;
 const refreshSecretKey = process.env.rkey; // Add a separate secret key for refresh tokens
